347b5a8ef0ce08ecd5ea564afbb886acd2b55fd6
Konrad Rudnicki <zeglarz@gmail.com>
2020-01-12 13:50:20 +0100

    add sql server connection

---
+++ api/db/connection.js
@@ -0,0 +1,12 @@
+const { Pool, Client } = require('pg');
+const config = require('../config.json');
+
+const client = new Client(config.DB);
+
+client.connect();
+
+module.exports = client;
+
+client.query('SELECT NOW()', (err, res) => {
+  console.log('db connected');
+});
