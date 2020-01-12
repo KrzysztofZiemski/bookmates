ad3ecc158fcd469d679ed1421571b6ca6e507ee4
krzysztof <k.b.ziemski@gmail.com>
2020-01-12 15:38:20 +0100

    add userBookModel

---
+++ api/db/models/userBookModel.js
@@ -0,0 +1,21 @@
+const connection=require('../connection');
+
+const tableName = 'user_books';
+
+const insertBook=(book)=>{
+    const{userID,title,author,isbn,genre,rating,status}=book
+    const sql = `
+        INSERT INTO ${tableName}
+        VALUES(
+           ${userID}, DEFAULT ,${title},${author},${isbn},${genre},${rating}, ${status}
+        )
+    `
+    return connection.query(sql)
+}
+
+const getBook=()=>{
+    const sql = `
+        SELECT * FROM ${tableName}
+    `
+    return connection.query(sql)
+}
