51348661f6684020b25737ac9198bacabebaca7b
Konrad Rudnicki <zeglarz@gmail.com>
2020-01-29 13:17:50 +0100

    dashboard page

--- src/modules/pages/dashboardPage/index.js
+++ src/modules/pages/dashboardPage/index.js
@@ -1,7 +1,81 @@
-import React from "react";
+import React, {useState} from "react";
+import {SearchArea} from "./searchArea";
+import request from 'superagent';
+import {BookList} from "./bookList";
+import axios from "axios";
+
 
 const Dashboard = () => {
-  return <div>dashboard</div>;
-};
+  let [books, setBooks] = useState([]);
+  let [searchField, setSearchField] = useState('');
+  let [sort, setSort] = useState('');
+
+  const cleanData = (data) => {
+    const cleanedData = data.body.items.map((book) => {
+      if (!book.volumeInfo.hasOwnProperty('publishedDate')) {
+        book.volumeInfo['publishedDate'] = '0000';
+     }
+      if (!book.volumeInfo.hasOwnProperty('imageLinks')) {
+      book.volumeInfo['imageLinks'] = {thumbnail: 'http://vrephotels.com/images/NoImageFound.png'};
+    }
+      if (!book.volumeInfo.hasOwnProperty('authors')){
+        book.volumeInfo['authors'] = ['not found'];
+      }
+      return book;
+
+
+    });
+    return cleanedData;
+  };
+
+  const searchBook = (e) => {
+    e.preventDefault();
+    request
+        .get("https://www.googleapis.com/books/v1/volumes")
+        .query({ q: searchField, maxResults: 20})
+        .then(data => {
+          console.log(data);
+          setBooks(cleanData(data));
+        });
+  };
+  // const searchBook = (e) => {
+  //   e.preventDefault();
+  //   request
+  // .get("https://data.bn.org.pl/api/bibs.json")
+  //       .query({title: searchField})
+  //       .then(data => setBooks([data.bibs]));
+  // }
+  // const searchBook = (e) => {
+  //   e.preventDefault();
+  //   axios
+  //       .get(`https://data.bn.org.pl/api/bibs.json?title=${searchField}`)
+  //       .then(data => setBooks([...data.bibs]));
+  // }
+
+  const handleSearch = (e) => {
+    setSearchField(e.target.value);
+  }
+
+  const handleSort= (e) => {
+    console.log(e.target.value);
+    setSort(e.target.value);
+  };
+
+const sortedBooks = books.sort((a, b) => {
+    if (sort === 'Newest') {
+      return parseInt(b.volumeInfo.publishedDate.substring(0,4)) - parseInt(a.volumeInfo.publishedDate.substring(0,4));
+    }
+  else if (sort === 'Oldest') {
+    return parseInt(a.volumeInfo.publishedDate.substring(0,4)) - parseInt(b.volumeInfo.publishedDate.substring(0,4));
+  }
+});
+
+  return (
+      <div>
+    <SearchArea handleSearch={handleSearch} searchBook={searchBook} handleSort={handleSort}/>
+    <BookList books={sortedBooks}/>
+  </div>
+)};
+
 
 export default Dashboard;
