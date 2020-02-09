c98b4a2ec816fe07dbb026b9171c02da2b0ab28d
Konrad Rudnicki <zeglarz@gmail.com>
2020-02-09 13:42:13 +0100

    changes

--- api/db/connection.js
+++ api/db/connection.js
@@ -8,5 +8,9 @@ client.connect();
 module.exports = client;
 
 client.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
   console.log('db connected');
+    }
 });
