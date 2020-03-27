805f1d7b96f95a2db309dba960ef118c1f0a0475
Paula Kuwałek <paula.kuwalek@gmail.com>
2020-03-27 16:39:54 +0100

    welcome page background image and content

--- src/modules/pages/welcomePage/welcomePage.js
+++ src/modules/pages/welcomePage/welcomePage.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./welcomePage.scss";
+import { ButtonBasic } from "./../../Button/Button";
 import { auth } from '../../../repos/user';
 import { setCookie } from '../../cookies/cookies';
 
@@ -6,9 +8,14 @@ import { setCookie } from '../../cookies/cookies';
 const WelcomePage = (props) => {
  
   return (
-    <div>
-      <h1>Welcome</h1>
-    </div>);
+    <div className="welcomePage">
+      <div className="wrapper">
+        <h1 className="logo">BookMates</h1>
+        <p className="companyMessage">książki łączą ludzi</p>
+        <ButtonBasic className="joinBtn" content="dołącz do społeczności" />
+      </div>
+    </div>
+  );
 };
 
 export default WelcomePage;
